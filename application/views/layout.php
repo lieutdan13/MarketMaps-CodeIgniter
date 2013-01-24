b8e103005cba6cdb812187631b56d6af25076e36
Dan Schaefer <dan@schaeferzone.net>
2013-01-23 23:03:15 -0500

    Adding necessary file for layout

---
+++ application/views/layout.php
@@ -0,0 +1,18 @@
+<?php echo "<!DOCTYPE html>"?>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+        <meta name="viewport" content="width=device-width" />
+        <title><?php echo $pageTitle?></title>
+        <!--[if IE 9]>
+        <style type="text/css"> .gradient { filter: none;} </style>
+        <![endif]-->
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <?php echo $content?>
+    </body>
+</html>
