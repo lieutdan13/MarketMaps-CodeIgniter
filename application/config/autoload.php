8ee971bdbf33ad896bf3c12dc623146c12850cbe
Dan Schaefer <dan@schaeferzone.net>
2013-02-18 07:07:41 -0500

    Autoloading Ion-Auth package

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -37,7 +37,10 @@
 |
 */
 
-$autoload['packages'] = array(APPPATH . 'third_party/asset-helper/');
+$autoload['packages'] = array(
+    APPPATH . 'third_party/asset-helper/',
+    APPPATH.'third_party/ion_auth',
+);
 
 
 /*
@@ -64,7 +67,7 @@ $autoload['libraries'] = array('database', 'session', 'ion_auth');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'asset');
+$autoload['helper'] = array('url', 'form', 'asset');
 
 
 /*
